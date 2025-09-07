21b31c63f47462a44b7b7c7c5531ff9db947f8b7
rctz <storyofdeath2541@gmail.com>
2025-09-07 11:11:40 +0700

    feat: Enhance logging for feature-B-01 in app.js

--- src/app.js
+++ src/app.js
@@ -4,3 +4,11 @@ console.log("Dev feature-B_3");
 console.log("Dev CORE feature-B_1");
 console.log("Dev CORE feature-B_2");
 console.log("Dev CORE feature-B_3");
+
+// ================= Feature B-01 =================
+console.log("Initial feature-B-01_1");
+console.log("Dev feature-B-01_2");
+console.log("Dev feature-B-01_3");
+console.log("Dev CORE feature-B-01_1");
+console.log("Dev CORE feature-B-01_2");
+console.log("Dev CORE feature-B-01_3");
