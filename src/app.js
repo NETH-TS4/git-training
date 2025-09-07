b0f28d5b5fa2bb959f207149bf54228e4a46bc03
rctz <storyofdeath2541@gmail.com>
2025-09-07 12:50:00 +0700

    feat: Add logging for feature-B-04 development in app.js

--- src/app.js
+++ src/app.js
@@ -17,3 +17,15 @@ console.log("Dev CORE feature-B-02_4");
 // === This block should be on prod ===
 console.log("Hello Workshop B");
 // === This block should be on prod ===
+
+// ================= Feature B-04 =================
+console.log("Initial feature-B-04_1");
+console.log("Dev feature-B-04_2");
+console.log("Dev feature-B-04_3");
+console.log("Dev CORE feature-B-04_1");
+console.log("Dev CORE feature-B-04_2");
+console.log("Dev CORE feature-B-04_3");
+console.log("Dev CORE feature-B-04_4");
+console.log("Dev CORE feature-B-04_5");
+console.log("Dev CORE feature-B-04_6");
+console.log("Dev CORE feature-B-04_7");
