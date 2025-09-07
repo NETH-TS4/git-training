a861bcb820e8a96ba329734141376903c3d2500b
rctz <storyofdeath2541@gmail.com>
2025-09-07 10:57:50 +0700

    feat: add core feature

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 console.log("Initial feature-A_1");
 console.log("Dev feature-A_2");
+console.log("Dev CORE feature-A_1");
+console.log("Dev CORE feature-A_2");
+console.log("Dev CORE feature-A_3");
