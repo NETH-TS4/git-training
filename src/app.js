a0b969c26781919974e387fdbd54f5e3e2d656d4
rctz <storyofdeath2541@gmail.com>
2025-09-07 11:08:42 +0700

    feat: Add initial logging for feature-B development in app.js

---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log("Initial feature-B_1");
+console.log("Dev feature-B_2");
+console.log("Dev feature-B_3");
+console.log("Dev CORE feature-B_1");
+console.log("Dev CORE feature-B_2");
+console.log("Dev CORE feature-B_3");
